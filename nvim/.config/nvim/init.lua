6a0b2fa78f9e3caba31146cb1516b4079d7a1eb7
Josh <joshuamatthewwalters@gmail.com>
2026-09-11 18:22:29 +0100

    Created nvim structure and basic init.lua configuration file

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,13 @@
+-- Autocomplete
+vim.opt.autocomplete = true
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Tab
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
