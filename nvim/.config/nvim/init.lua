0af7546d543c8f8b6a10e2cebf58fc9dabb76425
Josh <joshuamatthewwalters@gmail.com>
2026-09-12 08:30:18 +0100

    Modularise configuration file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,17 +1,1 @@
--- Autocomplete
-vim.opt.autocomplete = true
-vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
-
--- Line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
-
--- Search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Tab
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+require("config.options")
